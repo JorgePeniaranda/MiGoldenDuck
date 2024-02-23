d8f349ae3006bdf359659bb7478fc8dbb96fb9f8
jorgepeniaranda <peniarandajorge@gmail.com>
2024-02-23 16:16:37 -0300

    feat: create loader

---
+++ src/components/pages/loader/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Image, StyleSheet, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { getBaseColors } from '../../../const'
+import { Logos } from '../../../assets/images'
+
+export default function Loader() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={Logos.GoldenDuck}/>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: getBaseColors().PRIMARY,
+  }
+})
